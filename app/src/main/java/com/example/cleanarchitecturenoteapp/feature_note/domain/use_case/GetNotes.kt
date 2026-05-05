4ba43c256b8952cf3279a24529ccb52f6164e91b
Sanjeev Saju <sanjeevsaju@gmail.com>
2026-05-05 12:46:57 +0530

    feat: implement note taking with clean architecture

---
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotes.kt
@@ -0,0 +1,33 @@
+package com.example.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.example.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.example.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.example.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.example.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.util.Locale
+import java.util.Locale.getDefault
+
+class GetNotes(private val repository: NoteRepository) {
+    operator fun invoke(noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)) : Flow<List<Note>> {
+        return repository.getNotes().map { notes ->
+            when(noteOrder.orderType) {
+                is OrderType.Ascending -> {
+                    when(noteOrder) {
+                        is NoteOrder.Date -> notes.sortedBy { it.timestamp }
+                        is NoteOrder.Title -> notes.sortedBy { it.title.lowercase(getDefault()) }
+                        is NoteOrder.Color -> notes.sortedBy { it.color }
+                    }
+                }
+                is OrderType.Descending -> {
+                    when(noteOrder) {
+                        is NoteOrder.Date -> notes.sortedByDescending { it.timestamp }
+                        is NoteOrder.Title -> notes.sortedByDescending { it.title.lowercase(getDefault()) }
+                        is NoteOrder.Color -> notes.sortedByDescending { it.color }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,56 @@
+package com.example.cleanarchitecturenoteapp.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.example.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    onOrderChange : (NoteOrder) -> Unit
+) {
+    Column(modifier = modifier) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            DefaultRadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelected = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Date",
+                selected = noteOrder is NoteOrder.Date,
+                onSelected = { onOrderChange(NoteOrder.Date(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Color",
+                selected = noteOrder is NoteOrder.Color,
+                onSelected = { onOrderChange(NoteOrder.Color(noteOrder.orderType)) }
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            DefaultRadioButton(
+                text = "Ascending",
+                selected = noteOrder.orderType is OrderType.Ascending,
+                onSelected = { onOrderChange(noteOrder.copy(OrderType.Ascending)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Descending",
+                selected = noteOrder.orderType is OrderType.Descending,
+                onSelected = { onOrderChange(noteOrder.copy(OrderType.Descending)) }
+            )
+        }
+    }
+}
